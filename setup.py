8294150b5c11f17cc8330e8c29cec1a4194ce797
WGussev <wsewolodgusew@yandex.ru>
2020-06-03 21:33:47 +0300

    fix required pynvml version

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pynvml>=8.0.4"],
+    install_requires=["pynvml==8.0.4"],
     python_requires='>=3.6',
 )
